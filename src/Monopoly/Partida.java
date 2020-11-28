8a7a1fd30605ae5f7fdd671d5c00ddbb81102de5
Victor Coman <y0100848@tu-braunschweig.de>
2020-11-28 20:37:50 +0100

    fasilito i--

--- src/Monopoly/Partida.java
+++ src/Monopoly/Partida.java
@@ -3,7 +3,22 @@ package Monopoly;
 import java.util.ArrayList;
 
 public class Partida {
+	private int id_partida;
 	private int turno;
 	private Tablero tablero;
 	private ArrayList<Jugador> jugadores;
+	private static final int MAXJUGADORES = 8;
+	
+	public int getDados(){
+		
+		return 0;
+	}
+	
+	public boolean dadosIguales() {
+		
+		return true;
+	}
+	
+	
+	
 }
--- src/Monopoly/Tablero.java
+++ src/Monopoly/Tablero.java
@@ -1,8 +1,21 @@
 package Monopoly;
 
+import java.util.ArrayList;
+
 public class Tablero {
 	
 	//Jorge : las posiciones del tablero van de la 0 a la 39 , hay 40 casillas en el tablero
+
+	 static final int tam=39;
+	 
+	 private Casilla[] casillas = new Casilla[40];
+	 private ArrayList<String> baraja_comunidad = new ArrayList<String>();
+	 private ArrayList<String> baraja_sorpresa = new ArrayList<String>();
+	 
+	 
+	 
+	 
+	 
+	 
 	
-	static final int tam=39;
 }
