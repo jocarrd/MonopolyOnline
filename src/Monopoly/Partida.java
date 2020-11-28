f75a84bf2701bba103e97c8fa96c4ab0ff9f646f
Jocarrd <jocarrd@unirioja.es>
2020-11-28 01:15:19 +0100

    Clases creadas , primer planteamiento

--- src/Monopoly/Partida.java
+++ src/Monopoly/Partida.java
@@ -1,5 +1,9 @@
 package Monopoly;
 
-public class Partida {
+import java.util.ArrayList;
 
+public class Partida {
+	private int turno;
+	private Tablero tablero;
+	private ArrayList<Jugador> jugadores;
 }
--- src/Monopoly/Tablero.java
+++ src/Monopoly/Tablero.java
@@ -1,5 +1,7 @@
 package Monopoly;
 
 public class Tablero {
-
+	
+	//Jorge : las posiciones del tablero van de la 0 a la 39 , hay 40 casillas en el tablero
+	 static final int tam=39;
 }
