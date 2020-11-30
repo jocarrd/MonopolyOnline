23251577d6534d31caa03cf783f428ad19f1373a
Victor Coman <y0100848@tu-braunschweig.de>
2020-11-30 01:29:49 +0100

    pr

--- src/Monopoly/Calle.java
+++ src/Monopoly/Calle.java
@@ -6,13 +6,18 @@ import java.awt.Image;
 public class Calle extends Casilla {
 	private Color color;
 	private double precio_compra;
-	private double precio_venta;
+	private double alquiler;
+	private boolean tienePropietario = false;
+	//private double precio_venta;  yo lo llamaria alquiler
 
-	public Calle(int numero, String nombre, Color c, double compra, double venta) {
+
+	public Calle(int numero, String nombre, Color c, double compra, double venta, boolean tienePropietario) {
 		super(numero, nombre, TipoCasilla.calle);
 		this.color = c;
 		this.precio_compra = compra;
-		this.precio_venta = venta;
+		this.alquiler = venta;
+		this.tienePropietario = tienePropietario;
+		//this.precio_venta = venta;
 
 	}
 
@@ -32,12 +37,29 @@ public class Calle extends Casilla {
 		this.precio_compra = precio_compra;
 	}
 
-	public double getPrecio_venta() {
-		return precio_venta;
+	public double getAlquiler() {
+		return alquiler;
 	}
 
-	public void setPrecio_venta(double precio_venta) {
-		this.precio_venta = precio_venta;
+	public void setAlquiler(double alquiler) {
+		this.alquiler = alquiler;
 	}
+	
+	public boolean getTienePropietario() {
+		return tienePropietario;
+	}
+
+	public void setTienePropietario(boolean tienePropietario) {
+		this.tienePropietario = tienePropietario;
+	}
+	
+
+//	public double getPrecio_venta() {
+//		return precio_venta;
+//	}
+//
+//	public void setPrecio_venta(double precio_venta) {
+//		this.precio_venta = precio_venta;
+//	}
 
 }
