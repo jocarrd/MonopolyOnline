28d016a39aeea27dc5dc6bd5ce6cfa9cbb1f02a7
Jocarrd <jocarrd@unirioja.es>
2020-12-01 14:42:58 +0100

    Panel Registro

--- src/InterfazGrafica/probando.java
+++ src/InterfazGrafica/probando.java
@@ -2,7 +2,7 @@ package InterfazGrafica;
 
 public class probando {
 	public static void main(String []args) {
-		Panel_registro a = new Panel_registro();
+		PanelRegistro a = new PanelRegistro();
 		
 	}
 }
