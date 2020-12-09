f2e293ba5de641083d0ef34c2c00cac0ddfd0406
vicoman99 <43779372+vicoman99@users.noreply.github.com>
2020-12-10 00:37:07 +0100

    hola
    ff

--- src/Online/Servidor.java
+++ src/Online/Servidor.java
@@ -25,32 +25,33 @@ public class Servidor {
 
 		try (ServerSocket c = new ServerSocket(5555)) {
 			ExecutorService sesiones = Executors.newCachedThreadPool();
+			//NO METAS MAS DE 5 PARTIDAS sino F
 			List<Partida> partidas = new ArrayList<>();
 			Partida v1 = new Partida("1");
+			Partida v2 = new Partida("2");
 			Partida v3 = new Partida("3");
 			Partida v4 = new Partida("4");
-			partidas.add( v1);
-			partidas.add( v3);
-			partidas.add( v4);
+			Partida v5 = new Partida("5");
+			
+			partidas.add(v1);
+			partidas.add(v2);
+			partidas.add(v3);
+			partidas.add(v4);
+			partidas.add(v5);
+
 			while (true) {
 
 				try {
-					 
+
 					Socket cliente = c.accept();
-					
-					DataInputStream  ent =  new DataInputStream (cliente.getInputStream());
-					
-				
-					
-						if(ent.readLine().equals("inicio")) {
-							ObjectOutputStream  s=  new ObjectOutputStream (cliente.getOutputStream());	
-							s.writeObject(partidas);
-							s.flush();
-						}
-						
-					
-					
-					
+
+					DataInputStream ent = new DataInputStream(cliente.getInputStream());
+
+					if (ent.readLine().equals("inicio")) {
+						ObjectOutputStream s = new ObjectOutputStream(cliente.getOutputStream());
+						s.writeObject(partidas);
+						s.flush();
+					}
 
 				} catch (IOException e) {
 					e.printStackTrace();
